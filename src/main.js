8b53353ac18f52544c70d6c7937ad9d1f182c7a9
“ <755674130@qq.com>
2017-02-02 16:50:38 +0800

    因多组件需要数据共享，试用vuex,提前备份

--- src/main.js
+++ src/main.js
@@ -43,6 +43,6 @@ const router = new VueRouter({
 // 现在我们可以启动应用了！
 // 路由器会创建一个 App 实例，并且挂载到选择符 #app 匹配的元素上。
 const app = new Vue({
-  router: router,
+  router,
   render: h => h(App)
 }).$mount('#app')
